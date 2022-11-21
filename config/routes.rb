9df2a84748c5d29425c1d8f08b5da45b80d5e908
John Sagittary <johnsagittary@gmail.com>
2022-11-21 15:51:33 +0100

    added routes for devs and jobs

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :devs, only: [:index, :show] do
+    resources :jobs, only: [:new, :create]
+  end
+
 end
