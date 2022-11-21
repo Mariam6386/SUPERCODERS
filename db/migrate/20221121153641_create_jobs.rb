8c994f46a845ff88e16a80a6920684fa3a10023b
Susanne Lambregts <susanne.lambr@icloud.com>
2022-11-21 16:40:12 +0100

    generated dev and job model

---
+++ db/migrate/20221121153641_create_jobs.rb
@@ -0,0 +1,13 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.integer :completed
+      t.string :title
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+      t.references :dev, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
