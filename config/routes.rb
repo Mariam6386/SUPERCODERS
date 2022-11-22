e89c49d3b0d24155359fea8e37e622a25ca24a2d
MaxSeck <maxseck@hotmail.de>
2022-11-22 16:46:22 +0100

    feat: Create Dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Defines the root path route ("/")
+  # root "articles#index"
   get "devs/new", to: "devs#new"
   post "devs", to: "devs#create"
 
   resources :devs, only: [:index, :show, :new, :create] do
     resources :jobs, only: [:new, :create]
   end
+
 end
