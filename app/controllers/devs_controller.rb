23f47dc95fbb36e2e2f45e1737e60caafa3485e2
MaxSeck <maxseck@hotmail.de>
2022-11-22 16:11:13 +0100

    new_dev

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -2,4 +2,16 @@ class DevsController < ApplicationController
   def new
     @dev = Dev.new
   end
+
+  def create
+    @dev = Dev.new(dev_params)
+    @dev.save
+    redirect_to dev_path(@dev)
+  end
+
+  private
+
+  def dev_params
+    params.require(:dev).permit(:name, :github_link, :description, :price, :skills )
+  end
 end
